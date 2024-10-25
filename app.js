7333d7dd860f0364e5730bff2f5362e9df6abf6e
SeungYeop Yang <seungyeop.yang@gmail.com>
2024-10-25 11:15:30 -0500

    trimming the comments

--- app.js
+++ app.js
@@ -17,13 +17,6 @@ app.use("/api/v1/", templateRoutes);
 
 const { EXPRESS_PORT } = process.env;
 
-// const todoRoutes = require("./routes/todo.routes");
-// app.use("/todos", todoRoutes);
-
-app.get("/api/v1/hello", (req, res) => {
-    res.send("hello, from the earth");
-});
-
 app.get("/", (req, res) => {
     res.send(`<h1>Template App Running at ${EXPRESS_PORT}</h1>`);
 });
