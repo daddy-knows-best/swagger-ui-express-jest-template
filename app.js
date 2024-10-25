35cc08d8f962b452dbfcf74d8621f0cc43a7350e
SeungYeop Yang <seungyeop.yang@gmail.com>
2024-10-25 10:51:32 -0500

    routes

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ const express = require('express');
 const swaggerUI = require('swagger-ui-express');
 const YAML = require('yamljs');
 const swaggerDocument = YAML.load("./swagger.yaml");
+const templateRoutes = require('./routes/template-routes');
 
 const app = express();
 
 //middleware
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use(express.json());
+app.use("/api/v1/", templateRoutes);
 
 const { EXPRESS_PORT } = process.env;
 
